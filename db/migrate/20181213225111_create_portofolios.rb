6df22fba18c0bb35df3770a780d64fb1275d4023
seiyachida <wa718077@gmail.com>
2018-12-14 07:54:29 +0900

    Added portfolio via resource generator

---
+++ db/migrate/20181213225111_create_portofolios.rb
@@ -0,0 +1,12 @@
+class CreatePortofolios < ActiveRecord::Migration[5.2]
+  def change
+    create_table :portofolios do |t|
+      t.string :title
+      t.text :body
+      t.text :main_image
+      t.text :thumb_image
+
+      t.timestamps
+    end
+  end
+end
